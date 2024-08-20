514e0d997dee8ea597671c6fcc35c7770ed9bb32
TAJAMUL ARAFATH V P <CIPL1524@colanonline.local>
2024-08-20 20:06:00 +0530

    Added Root Module

--- root/build.gradle.kts
+++ root/build.gradle.kts
@@ -1,24 +1,17 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
 }
 
 android {
-    namespace = "com.example.hellodr"
+    namespace = "com.example.root"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.hellodr"
         minSdk = 28
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildFeatures{
-        viewBinding = true
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -44,18 +37,9 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation ("androidx.viewpager2:viewpager2:1.1.0")
-
-    //    Splash Screen Dependency
-    implementation ("androidx.core:core-splashscreen:1.0.1")
-
-    //Added root module
-    implementation(project(":root"))
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "Hello DR"
 include(":app")
- 
+include(":root")
